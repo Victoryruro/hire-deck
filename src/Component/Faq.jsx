1ee0bbe9b85b2947fb614e4c95c14fade1f821c8
Victory Diemuodeke <Victorydiemuodeke@gmail.com>
2026-09-04 14:55:18 +0100

    Faq fixed

--- src/Component/Faq.jsx
+++ src/Component/Faq.jsx
@@ -41,7 +41,7 @@ const Faq = () => {
                 transition={{ duration: 0.7, delay: 2 * 0.1, ease: 'easeOut' }}
                 className="w-full md:w-full lg:w-3/4 mt-10 group [&amp;_summary::-webkit-details-marker]:hidden bg-primary-400 dark:bg-primary-500" open=""  >
                 <summary className="flex items-center justify-between gap-1.5 rounded-md border border-gray-100  p-6 text-gray-900 dark:border-gray-700! dark:bg-gray-800! dark:text-white!">
-                <h2 className="text-2xl font-medium">How do I post a job on HireHub </h2>
+                <h2 className="text-2xl font-medium">How do I post a job on HireDesk </h2>
 
                 <svg xmlns="http://www.w3.org/2000/svg" className="block size-5 shrink-0 group-open:hidden" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3m0 0v3m0-3h3m-3 0H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"></path>
